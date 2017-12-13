569273364517c71f508103c9d2521de840e1d7cf
regbake <regbake@gmail.com>
2017-12-13 10:25:54 -0800

    hooked up cheerio, cannot get p tags directly

--- routes/scrape.js
+++ routes/scrape.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var superagent = require('superagent')
+var cheerio = require('cheerio')
 
 router.get('/', function(req, res, next) {
   var url = 'https://www.theatlantic.com/magazine/archive/2018/01/the-new-testament-a-translation-david-bentley-hart/546551/'
@@ -19,6 +20,12 @@ router.get('/', function(req, res, next) {
       })
       return
     }
+
+    $ = cheerio.load(response.text)
+    $('<p>').each(function(i, element){
+      console.log(element)
+    })
+
     res.send(response.text)
   })
 
